4a8e0f96849c747ebbda063df5cd44ab1da62463
Murthy Vemuri <murthyvemuri211@gmail.com>
2015-09-30 06:39:07 +0530

    Create 10-1.c

---
+++ 10-1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int i,n,x,sum=0;
+int a;
+char ar[26]=
+{'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',};
+
+scanf("%d",&n);
+
+}
+
