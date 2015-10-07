9a0a49dc389f3c32a3f1724aaf73158f6ce963a7
Murthy Vemuri <murthyvemuri211@gmail.com>
2015-10-07 06:47:11 +0530

    Create stringcompare.c

---
+++ stringcompare.c
@@ -0,0 +1,39 @@
+
+
+#include <stdio.h>
+ 
+int compare_strings(char [], char []); 
+ 
+int main()
+{
+   int flag;
+   char a[1000], b[1000];
+   printf("Input first string\n");
+   scanf("%s",a);
+   printf("Input second string\n");
+   scanf("%s",b);
+ 
+   flag = compare_strings(a, b);
+ 
+   if (flag == 0)
+      printf("strings are equal.\n");
+   else
+      printf("strings are not equal.\n");
+ 
+   return 0;
+}
+ 
+int compare_strings(char a[], char b[])
+{
+   int c = 0;
+   while (a[c] == b[c])
+   {
+      if(a[c]=='\0'||b[c]=='\0')
+      break;
+      c++;
+   }
+   if (a[c] == '\0' && b[c] == '\0')
+      return 0;
+   else
+      return -1;
+}
