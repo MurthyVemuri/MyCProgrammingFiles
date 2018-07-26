db1e61e9581473dafaeb3a38b0f61804b14425df
MurthyVemuri <murthyvemuri211@gmail.com>
2018-07-26 11:10:27 +0530

    updated imahero.c file for fun.

--- iamhero.c
+++ iamhero.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
-void main()
+int main()
 {
 char line[81], ctr;
-int i,c,
-end = 0,
-characters = 0,
-words = 0,
-lines = 0;
+int c,characters = 0,words = 0,lines = 0;
 printf("KEY IN THE TEXT.\n");
 printf("GIVE ONE SPACE AFTER EACH WORD.\n");
 printf("WHEN COMPLETED, PRESS 'RETURN'.\n\n");
-while( end == 0)
+while( 1)
 {
 c = 0;
 		while((ctr=getchar()) != '\n')
@@ -22,7 +18,7 @@ c = 0;
 		else
 		{
 			words++;
-		for(i=0; line[i] != '\0';i++)
+		for(int i=0; line[i] != '\0';i++)
 			if(line[i] == ' ' || line[i] == '\t')
 			words++;
 		}
@@ -34,4 +30,5 @@ c = 0;
 	printf("Number of lines = %d\n", lines);
 	printf("Number of words = %d\n", words);
 	printf("Number of characters = %d\n", characters);
+	return 0;
 }
